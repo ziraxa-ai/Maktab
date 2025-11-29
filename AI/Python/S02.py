498af12c32957c11f356e5aa2f5fc374906994aa
Taha Saadat <mrtahasaadat@gmail.com>
2025-11-29 12:31:22 +0330

    Add files via upload

---
+++ AI/Python/S02.py
@@ -0,0 +1,25 @@
+#EX 01 
+
+age = int(input("Enter your age: "))
+
+if age  >= 18:
+    print("You are an adult.")
+
+else:
+    print("You are a minor.")
+
+## EX 02
+
+score = int(input("Enter your score: "))
+if score >= 90:
+    print("Grade: A")
+if score >= 75:
+    print("Grade: B")
+if score >= 60:
+    print("Grade: C")
+else:
+    print("Grade: F")
+
+
+# EX 03
+
