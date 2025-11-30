85f583b1d2a6b1aad8f09201d3e8fbb51b114459
Taha Saadat <mrtahasaadat@gmail.com>
2025-11-30 15:27:22 +0330

    Add files via upload

--- AI/Python/S02.py
+++ AI/Python/S02.py
@@ -37,6 +37,11 @@ else:
 
 #EX 04
 
+
+# elif = > اما اگر
+
+# EX 05
+
 print("Welcome to contact list .")
 
 contact_name = input("Enter contact name: ")
@@ -58,7 +63,7 @@ else:
 
 
 
-#EX 05
+#EX 06
 
 
 print("Welcome to contact list .")
@@ -67,5 +72,106 @@ contact_name = input("Enter contact name: ")
 contact_family = input("Enter contact family: ")
 contact_age = int(input("Enter contact age: "))
 
-while 
+if contact_name == "":
+    print("Contact name is required.")
+    contact_name = input("Enter contact name: ")
+
+if contact_family == "":
+    print("Contact family is required.")
+    contact_family = input("Enter contact family: ")
+
+if contact_age <= 0:
+    print("Invalid age. Age must be positive.")
+    contact_age = int(input("Enter contact age: "))
+
+else:
+    res = input("View profile? (yes/no): ")
+    if res == "yes":
+        print("Contact Name:", contact_name)
+        print("Contact Family:", contact_family)
+        print("Contact Age:", contact_age)
+    elif res == "no":
+        print("Profile viewing canceled.")
+
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        res1 = input("View profile? (yes/no): ")
+
+        if res1 == "yes":
+            print("Contact Name:", contact_name)
+            print("Contact Family:", contact_family)
+            print("Contact Age:", contact_age)
+
+        elif res1 == "no":
+            print("Profile viewing canceled.")
+        
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+# ----------------------------------------------------------------------------------------------
+
+# EX 07
+
+for i in range(1, 6):
+    print("Iteration:", i)
+
+
+
+# ----------------------------------------------------------------------------------------------
+password = ""
+
+while password != "admin123":
+    password = input("Enter password: ")
+
+print("Access Granted!")
+
+
+x = 10
+while x > 0:
+    print(x)
+    x -= 1
+
+# ----------------------------------------------------------------------------------------------
+# break, continue , pass
+# or , and , not
+
+
+# EX 08
+print("Welcome to contact list .")
+
+password = "1234"
+
+for i in range(3):
+    user_password = input("Enter password: ")
+
+    if user_password == password:
+        print("Access Granted!")
+        break
+    else:
+        print("Access Denied!")
+
+while True:
+    contact_name = input("Enter contact name: ")
+    contact_family = input("Enter contact family: ")
+    contact_age = int(input("Enter contact age: "))
+
+
+    if contact_name == "" and contact_family == "" and contact_age <= 0:
+        print("Contact name is required.")
+        continue
+
+    else:
+        break
+
+while True:
     license = input("View profile? (yes/no): ")
+
+    if license == "yes":
+        print("Contact Name:", contact_name)
+        print("Contact Family:", contact_family)
+        print("Contact Age:", contact_age)
+
+    elif license == "no":
+        print("Profile viewing canceled.")
+
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
